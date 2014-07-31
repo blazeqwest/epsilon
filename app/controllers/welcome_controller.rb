486b47adc62b72271db1ee86134a8e6c715fef45
Kostiantyn Nikolaiev <blazeqwest@ya.ru>
2014-07-31 17:54:45 +0300

    Fixed welcome layout and authorization requirements for home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :authenticate_user! 
   def index
   end
   protect_from_forgery with: :exception
