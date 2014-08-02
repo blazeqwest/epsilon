06e3459b13f27040f43390628cf70a912d374dd0
Kostiantyn Nikolaiev <blazeqwest@ya.ru>
2014-08-02 19:46:19 +0300

    Fixed minor issues with Welcome controller and default layout.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,4 @@
-class WelcomeController < ActionController::Base
+class WelcomeController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_action :authenticate_user!
