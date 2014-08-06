8fe5ab36ab229f2f849feadbb0158fabdb00425d
Kostiantyn Nikolaiev <blazeqwest@ya.ru>
2014-08-06 18:49:29 +0300

    Fixet spec for Account model

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -5,25 +5,25 @@ RSpec.describe Account, :type => :model do
     expect(FactoryGirl.build(:account)).to be_valid
   end
   it "should require a balance" do
-    expect(FactoryGirl.build(:account, balance: nil)).should_not be_valid
+    expect(FactoryGirl.build(:account, balance: nil)).not_to be_valid
   end
   it "should require a user_id" do
-    expect(FactoryGirl.build(:account, user_id: nil)).should_not be_valid
+    expect(FactoryGirl.build(:account, user_id: nil)).not_to be_valid
   end
   it "should require a name" do
-    expect(FactoryGirl.build(:account, name: nil)).should_not be_valid
+    expect(FactoryGirl.build(:account, name: nil)).not_to be_valid
   end
   it "should require a name with length more than 3 letters(valid case)" do
-    expect(FactoryGirl.build(:account, name: "ok")).should_not be_valid
+    expect(FactoryGirl.build(:account, name: "ok")).not_to be_valid
   end
   it "should require a name with length more than 3 letters(invalid case)" do
     expect(FactoryGirl.build(:account, name: "okies")).to be_valid
   end
   it "should require a balance to be numerical" do
-    expect(FactoryGirl.build(:account, balance: "ok")).should_not be_valid
+    expect(FactoryGirl.build(:account, balance: "ok")).not_to be_valid
   end
   it "should require a user_id to be numerical" do
-    expect(FactoryGirl.build(:account, user_id: "ok")).should_not be_valid
+    expect(FactoryGirl.build(:account, user_id: "ok")).not_to be_valid
   end
   it { is_expected.to have_many(:transactions) }
   it { is_expected.to belong_to(:user) }
