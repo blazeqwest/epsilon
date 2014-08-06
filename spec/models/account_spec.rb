c08e26fe8ffe59870c2f256df01dd4ac12577f4a
Kostiantyn Nikolaiev <blazeqwest@ya.ru>
2014-08-06 18:34:25 +0300

    spec for account model

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,9 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Account, :type => :model do
   it "account has a valid factory" do
-    expect( FactoryGirl.build(:account)).to be_valid
+    expect(FactoryGirl.build(:account)).to be_valid
   end
-
+  it "should require a balance" do
+    expect(FactoryGirl.build(:account, balance: nil)).should_not be_valid
+  end
+  it "should require a user_id" do
+    expect(FactoryGirl.build(:account, user_id: nil)).should_not be_valid
+  end
+  it "should require a name" do
+    expect(FactoryGirl.build(:account, name: nil)).should_not be_valid
+  end
+  it "should require a name with length more than 3 letters(valid case)" do
+    expect(FactoryGirl.build(:account, name: "ok")).should_not be_valid
+  end
+  it "should require a name with length more than 3 letters(invalid case)" do
+    expect(FactoryGirl.build(:account, name: "okies")).to be_valid
+  end
+  it "should require a balance to be numerical" do
+    expect(FactoryGirl.build(:account, balance: "ok")).should_not be_valid
+  end
+  it "should require a user_id to be numerical" do
+    expect(FactoryGirl.build(:account, user_id: "ok")).should_not be_valid
+  end
   it { is_expected.to have_many(:transactions) }
   it { is_expected.to belong_to(:user) }
 end
