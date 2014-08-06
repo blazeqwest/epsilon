a5933115d4e3e140cc7640de81d22fd11c330500
Kostiantyn Nikolaiev <blazeqwest@ya.ru>
2014-08-06 07:27:09 +0300

    Transactions CRUD - part two (update, delete)

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,16 +1,18 @@
 class TransactionsController < ApplicationController
   def new
   end
+
   def can_see?
     transaction = Transaction.find(params[:id])
     account = Account.find(params[:account_id])
-    acount.user_id == current_user.id
+    return account.user_id == current_user.id
   end
+  
   def create
     @transaction = Transaction.new(transaction_params)
     @transaction.account_id = params[:account_id]
     @transaction.save
-    @account = Account.find(4)
+    @account = Account.find(params[:account_id])
     @account.balance = @transaction.amount + @account.balance
     @account.save
     redirect_to @account
@@ -38,11 +40,32 @@ class TransactionsController < ApplicationController
     end
   end
 
+  def update
+    if can_see?
+      @transaction = Transaction.find(params[:id])
+      if @transaction.amount == params[:transaction][:amount]
+        redirect_to account_transactions_path
+      else
+        @account = Account.find(params[:account_id])
+        @account.balance = @account.balance - @transaction.amount + params[:transaction][:amount].to_f
+        @account.save
+        @transaction.amount = params[:transaction][:amount] 
+        @transaction.save
+        redirect_to account_transactions_path
+      end
+    else
+      render :status => :forbidden, :text => "Forbidden fruit"
+    end
+  end
+
   def destroy
     @transaction = Transaction.find(params[:id])
     if can_see?
+      @account = Account.find(params[:account_id])
+      @account.balance = @account.balance - @transaction.amount
+      @account.save
       @transaction.destroy
-      redirect_to accounts_transactions_path
+      redirect_to account_transactions_path
     else
       render :status => :forbidden, :text => "Forbidden fruit"
     end
